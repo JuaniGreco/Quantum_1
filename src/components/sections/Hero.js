340ce53e5f71176e944fde3007f7e635da9d22eb
Juani Greco <juanigreco22@gmail.com>
2022-06-01 13:59:18 -0300

    Initial fixes

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,45 +61,29 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              We provide webs for <span className="text-color-primary">startups</span> and <span className="text-color-primary">mid-level</span> companies
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Our webs works on all devices, so you only have to focus on your business.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                  <Button tag="a" color="primary" wideMobile href="mailto:quantumitweb@gmail.com?Subject=Hello, I need info">
+                    Contact Us
                     </Button>
                 </ButtonGroup>
               </div>
             </div>
           </div>
-          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
-            <a
-              data-video="https://player.vimeo.com/video/174002812"
-              href="#0"
-              aria-controls="video-modal"
-              onClick={openModal}
-            >
+          <div className="hero-figure reveal-from-bottom" data-reveal-value="20px" data-reveal-delay="800">
               <Image
                 className="has-shadow"
                 src={require('./../../assets/images/video-placeholder.jpg')}
-                alt="Hero"
+                alt="Image from the team"
                 width={896}
                 height={504} />
-            </a>
           </div>
-          <Modal
-            id="video-modal"
-            show={videoModalActive}
-            handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
-            videoTag="iframe" />
         </div>
       </div>
     </section>
